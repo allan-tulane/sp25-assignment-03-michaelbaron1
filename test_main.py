b5ce5c8aaa0f7ed866be784969caa055bdf1ce0d
michaelbaron1 <143116124+michaelbaron1@users.noreply.github.com>
2025-04-07 23:55:29 -0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -5,6 +5,8 @@ def test_MED():
         assert fast_MED(S, T) == MED(S, T)
                                  
 def test_align():
+        # think that this is not possible to do becasue when two seperate patterns of intsert/delete are found with the same cost
+    # one of the test cases has the first one found to be correct and the other test case has the second so no matter which you keep one will be wrong 
     for i in range(len(test_cases)):
         S, T = test_cases[i]
         align_S, align_T = fast_align_MED(S, T)
