3bd133924cee3460fdfef99e72618443286f4aef
michaelbaron1 <143116124+michaelbaron1@users.noreply.github.com>
2025-04-07 23:56:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -19,11 +19,69 @@ def MED(S, T):
             return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
 
 
-def fast_MED(S, T, MED={}):
-    # TODO -  implement top-down memoization
-    pass
 
-def fast_align_MED(S, T, MED={}):
-    # TODO - keep track of alignment
-    pass
+def fast_MED(S, T, MED={}):
+
+
+
+    # TODO -  implement top-down memoization
+    if (S, T) in MED:
+        return MED[(S,T)]
+
+    if S == "":
+        result = len(T)
+    elif T == "":
+        result = len(S)
+    elif S[0] == T[0]:
+        result = fast_MED(S[1:], T[1:], MED)
+    else:
+
+        result = 1 + min(fast_MED(S, T[1:], MED), fast_MED(S[1:], T, MED))
+    MED[(S, T)] = result
+    return result
+
+
+
+
+    # Top-down memoized edit distance with alignment
+def fast_align_MED(S, T, MED=None):
+
+    top_level = False
+    if MED is None:
+        MED = {}
+        top_level = True
+    if (S, T) in MED:
+        result = MED[(S, T)]
+    elif S == "":
+        result = (len(T), ("-" * len(T), T))
+    elif T == "":
+
+        result = (len(S), (S, "-" * len(S)))
+    elif S[0] == T[0]:
+
+        cost, (align_S, align_T) = fast_align_MED(S[1:], T[1:], MED)
+        result = (cost, (S[0] + align_S, T[0] + align_T))
+    else:
+
+        insert_cost, (ins_align_S, ins_align_T) = fast_align_MED(S, T[1:], MED)
+        option1 = (1 + insert_cost, ('-' + ins_align_S, T[0] + ins_align_T))
+
+
+        delete_cost, (del_align_S, del_align_T) = fast_align_MED(S[1:], T, MED)
+        option2 = (1 + delete_cost, (S[0] + del_align_S, '-' + del_align_T))
+
+
+        result = option1 if option1[0] <= option2[0] else option2
+
+    # Save the computed result for (S, T)
+    MED[(S, T)] = result
+
+    if top_level:
+        return result[1]
+    else:
+        return result
+
+
+
+
 
